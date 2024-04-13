2a6c6e59c5bc3ba0031da7152f1c2d88f964d769
Sherbek <mavlonovsherbek43@gmail.com>
2024-04-13 19:46:27 +0500

    fix: how it works section button size and image

--- src/components/How-it-works.vue
+++ src/components/How-it-works.vue
@@ -19,38 +19,36 @@ import ArrowRight from './icons/Arrow-right.vue'
 
       <div
         data-aos="fade-up"
-        class="flex flex-col md:flex-col lg:flex-col xl:flex-row items-center gap-10"
+        class="flex flex-col md:flex-col gap-28 lg:flex-col xl:flex-row items-center"
       >
         <div class="w-full lg:w-full xl:w-1/2">
-          <p
-            class="font text-xl lg:text-[30px] leading-7 lg:leading-[41px] mb-[50px] text-white"
-          >
+          <p class="font text-xl lg:text-[30px] leading-7 lg:leading-[41px] mb-[50px] text-white">
             The client is the software that you install on your device to connect to the VPN network
             and browse the web securely.
             <br />
             <br />
-            The node is the software that you install on your server to lend your internet connection to the
-            network, and in return, earn rewards.
+            The node is the software that you install on your server to lend your internet
+            connection to the network, and in return, earn rewards.
             <br />
             <br />
-            Our goal: to become the first L2-Chain for a decentralized P2P VPN, accessible worldwide,
-            without censorship and respecting your privacy, where clients bid for node
+            Our goal: to become the first L2-Chain for a decentralized P2P VPN, accessible
+            worldwide, without censorship and respecting your privacy, where clients bid for node
             usage, creating a decentralized VPN marketplace.
           </p>
           <a
             href="https://t.me/zerox_vpn_bot"
             target="_blank"
-            class="rounded-[27px] mt-[0px] text-white flex text-xl leading-[16px] items-center justify-between border border-white px-[30px] py-[12px] hover:bg-white hover:text-[#121829] duration-[0.3s] transition-all"
+            class="max-w-[300px] rounded-[27px] mt-[0px] text-white flex text-xl leading-[16px] items-center justify-between border border-white px-[30px] py-[12px] hover:bg-white hover:text-[#121829] duration-[0.3s] transition-all"
           >
             Become a Node
             <ArrowRight />
           </a>
         </div>
-        <div class="w-full lg:w-full xl:w-1/2">
+        <div class="w-full lg:w-full xl:w-1/2 mt-5">
           <div
             class="lg:w-full w-full xl:max-w-[600px] h-[600px] bg-[#1B255C] flex justify-center items-center rounded-[15px]"
           >
-            <img src="/P2P.png" alt="" class="w-full rounded-[15px]" />
+            <img src="/P2P.png" alt="" class="w-full h-full object-cover rounded-[15px]" />
           </div>
         </div>
       </div>
