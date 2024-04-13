a312843a5d6e0c6337234e312237b18dda5d1963
RealToma <real.toma.contact@gmail.com>
2024-04-13 17:59:53 -0400

    updated border

--- src/components/DesktopClient.vue
+++ src/components/DesktopClient.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts"></script>
 <template>
-  <section id="desktop-client" class="px-5 w-full pt-[100px] pb-[70px] bg-[#657fcb]">
+  <section id="desktop-client"
+    class="px-5 w-full pt-[100px] pb-[70px] bg-gradient-to-b from-[rgba(0,4,13,1)] to-[rgba(29,28,62,1)]">
     <div class="container lg:px-0">
-      <h1
-        class="text-center font-semibold leading-[87px] text-4xl lg:text-[64px] text-white"
-        data-aos="fade-up"
-      >
+      <h1 class="text-center font-semibold leading-[87px] text-4xl lg:text-[64px] text-white" data-aos="fade-up">
         0xVPN Desktop Client
       </h1>
-      <p
-        class="text-center font-medium text-xl lg:text-[26px] text-white mt-[28px] mb-[52px]"
-        data-aos="fade-up"
-      >
+      <p class="text-center font-medium text-xl lg:text-[26px] text-white mt-[28px] mb-[52px]" data-aos="fade-up">
         Our client is available for both Windows & Mac.
         <br /><br />
         This is a BETA version, please
@@ -19,21 +14,14 @@
         bugs to our team.
       </p>
 
-      <div
-        data-aos="fade-up"
-        class="w-full flex justify-center items-center lg:flex-row flex-col gap-[23px] pt-[50px]"
-      >
-        <a
-          href="https://download.0xvpn.org/0xVPN-Client-Setup-0.1.0.zip"
-          class="flex max-w-[340px] items-center hover:cursor-pointer hover:bg-[#FCBB14] hover:border-transparent duration-[0.3s] px-[38px] border border-black rounded-[23px] font-medium py-[13px] bg-black text-white text-base"
-        >
+      <div data-aos="fade-up" class="w-full flex justify-center items-center lg:flex-row flex-col gap-[23px] pt-[50px]">
+        <a href="https://download.0xvpn.org/0xVPN-Client-Setup-0.1.0.zip"
+          class="flex max-w-[340px] items-center hover:cursor-pointer hover:bg-[#FCBB14] hover:border-transparent duration-[0.3s] px-[38px] border border-black rounded-[23px] font-medium py-[13px] bg-black text-white text-base">
           <div class="flex mr-[5px]"><img src="/windows.png" width="20px" alt="mac" /></div>
           <div class="flex">&nbsp;Download for Windows (x64)</div>
         </a>
-        <a
-          href="https://download.0xvpn.org/0xVPN-Client-0.1.0-arm64.dmg"
-          class="flex items-center max-w-[340px] hover:cursor-pointer hover:bg-[#FCBB14] hover:border-transparent duration-[0.3s] px-[38px] border border-black rounded-[23px] font-medium py-[13px] bg-black text-white text-base"
-        >
+        <a href="https://download.0xvpn.org/0xVPN-Client-0.1.0-arm64.dmg"
+          class="flex items-center max-w-[340px] hover:cursor-pointer hover:bg-[#FCBB14] hover:border-transparent duration-[0.3s] px-[38px] border border-black rounded-[23px] font-medium py-[13px] bg-black text-white text-base">
           <div class="flex mr-[5px]"><img src="/mac.png" width="20px" alt="mac" /></div>
           <div class="flex">&nbsp;Download for Mac (M1/M2)</div>
         </a>
